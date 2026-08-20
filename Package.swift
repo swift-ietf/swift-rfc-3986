ca972c8fb49b390c673bf484bdf17511cbc54649
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:12:06 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-rfc-3986",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(
@@ -17,32 +17,59 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-parser-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-parser-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-standards/swift-ipv4-standard.git", branch: "main"),
-        .package(url: "https://github.com/swift-standards/swift-ipv6-standard.git", branch: "main")
+        .package(url: "https://github.com/swift-standards/swift-ipv6-standard.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "RFC 3986",
             dependencies: [
-                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .product(
+                    name: "Standard Library Extensions",
+                    package: "swift-standard-library-extensions"
+                ),
                 .product(name: "Parser Primitives", package: "swift-parser-primitives"),
                 .product(name: "ASCII Primitives", package: "swift-ascii-primitives"),
-                .product(name: "ASCII Serializer Primitives", package: "swift-ascii-serializer-primitives"),
-                .product(name: "ASCII Decimal Parser Primitives", package: "swift-ascii-parser-primitives"),
-                .product(name: "Parseable ASCII Primitives", package: "swift-ascii-parser-primitives"),
+                .product(
+                    name: "ASCII Serializer Primitives",
+                    package: "swift-ascii-serializer-primitives"
+                ),
+                .product(
+                    name: "ASCII Decimal Parser Primitives",
+                    package: "swift-ascii-parser-primitives"
+                ),
+                .product(
+                    name: "Parseable ASCII Primitives",
+                    package: "swift-ascii-parser-primitives"
+                ),
                 .product(name: "IPv4 Standard", package: "swift-ipv4-standard"),
-                .product(name: "IPv6 Standard", package: "swift-ipv6-standard")
+                .product(name: "IPv6 Standard", package: "swift-ipv6-standard"),
             ]
         ),
         .testTarget(
             name: "RFC 3986 Tests",
             dependencies: [
-                "RFC 3986",
+                "RFC 3986"
             ]
         ),
     ],
